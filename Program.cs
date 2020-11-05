2be004518585d953e32addf433930aee06e722d3
Olivia Persson <Olivia__P@hotmail.com>
2020-11-05 22:53:32 +0100

    Command to add new person, do while loop

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace AddressBook
             List<Person> addressBook = new List<Person>();
             string fileName = @"C:\Users\Olivi\adressbok.txt";
 
-            //Källa: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
+            //Source: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
             using (StreamReader file = new StreamReader(fileName))
             {
                 string line;
@@ -33,26 +33,43 @@ namespace AddressBook
                 while ((line = file.ReadLine()) != null)
                 {
                     string[] words = line.Split('#');
-                    Console.WriteLine("{0} {1}  {2}  {3}", words[0], words[1], words[2], words[3]); //Testutskrift
+                    Console.WriteLine("{0} {1}  {2}  {3}", words[0], words[1], words[2], words[3]); //TestPrint
                     addressBook.Add(new Person(words[0], words[1], words[2], words[3]));
                   
                 }
                 file.Close();
                 for (int i = 0; i < addressBook.Count(); i++)
                 {
-                    //Testutskrift
+                    //TestPrint
                     Console.WriteLine($"{addressBook[i].firstName} {addressBook[i].lastName}  {addressBook[i].number}  {addressBook[i].email}");
                 }
             }
             Console.WriteLine("Hej och välkommen till adressboken!");
             Console.WriteLine("Skriv 'sluta' om du vill avsluta programmet!");
-            Console.Write("> ");
-            string command = Console.ReadLine();
+            string command;
 
-            if (command == "sluta")
+            do
             {
-                Console.WriteLine("Hej då!");
-            }
+                Console.Write("> ");
+                command = Console.ReadLine();
+                if (command == "sluta")
+                {
+                    Console.WriteLine("Hej då!");
+                }
+                else if (command == "lägg till person")
+                {
+                    Console.Write("Skriv in förnamn: ");
+                    string firstName = Console.ReadLine();
+                    Console.Write("Skriv in efternamn: ");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Skriv in tel nummer: ");
+                    string number = Console.ReadLine();
+                    Console.Write("Skriv in email: ");
+                    string email = Console.ReadLine();
+                    Console.WriteLine($"namn: {firstName} {lastName} num: {number} email: {email}"); //TestPrint
+                    addressBook.Add(new Person(firstName, lastName, number, email));
+                }
+            } while (command != "sluta");
         }
     }
 }
