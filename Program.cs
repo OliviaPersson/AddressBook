d2d2ed82e739293ef7be9c1128040880155cf5e4
Olivia Persson <Olivia__P@hotmail.com>
2020-11-05 17:42:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Program
+    {
+        class Person
+        {
+            public string firstName, lastName, number, email;
+            public Person (string fName, string lName, string num, string em)
+            {
+                firstName = fName;
+                lastName = lName;
+                number = num;
+                email = em;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Person> addressBook = new List<Person>();
+            string fileName = @"C: \Users\Olivi\adressbok.txt";
+        }
+    }
+}
